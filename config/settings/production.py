e5f62ad3de245e6d8872679658ac38cac6299c02
Auguestine <crossxyj@gmail.com>
2016-08-24 22:12:16 +0800

    修复了一些缺少__init__.py 的问题

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,18 @@
 from .common import *
 
-
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*', ]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'a_web_db',
+        'USER': 'a_web_local',
+        'PASSWORD': '6789abc#',
+        'HOST': 'localhost',
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+# static root for collectstatic
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'a_web_static')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'a_web_media')
