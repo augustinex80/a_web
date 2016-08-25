194dbb889785bc35cb05422d7d38ad6f31ece885
Auguestine <crossxyj@gmail.com>
2016-08-25 14:10:45 +0800

    1054, "Unknown column 'posts_post.is_top' in 'where clause'" 提交到生产服务器(ubuntu12),发生了上面的错误,测试数据库为sqlite3, 生产数据库为mysql. 本地配置一个测试mysql数据库(mac os)没有错误发生. 清除数据,重新migrate之后问题不存在, mysql版本都是5.7,  条件所限,暂做记录.  修复了一些小细节,以后测试数据库改为mysql.

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,3 +2,15 @@ from .common import *
 
 DEBUG = True
 ALLOWED_HOSTS = ['*', ]
+
+# db
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_db',
+        'USER': 'test_user',
+        'PASSWORD': '123123',
+        'HOST': 'localhost',
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
